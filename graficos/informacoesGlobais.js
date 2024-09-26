5a16a0a65e236f018d19a4c9ae1e3198b7fb14ce
lorenaserbai20 <130658109+lorenaserbai20@users.noreply.github.com>
2024-09-26 11:13:54 -0300

    globais

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,8 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json' 
+
+async function visualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = res.json()
+    console.log(dados);
+}
+visualizarInformacoesGlobais()
