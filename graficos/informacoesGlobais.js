98aa6363f3e546dede555e33bcc151a78181f879
lorenaserbai20 <130658109+lorenaserbai20@users.noreply.github.com>
2024-09-27 11:55:27 -0300

    foi

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,6 +6,6 @@ async function visualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
     paragrafo.innerHTML = `Você sabia que o mundo ter ${dados.total_pessoas_mundos} de pesoas e que aproximadamente ${dados.total_pessoas_conectadas} etão conectadas em alguma rede social e passam em média ${dados.tempo.medio} horas conectadas.`
-    console.log(dados);
+    console.log(paragrafo);
 }
 visualizarInformacoesGlobais()
