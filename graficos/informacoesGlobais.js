d5815a4e12b35f84099cf4b7968079afe90d1620
lorenaserbai20 <130658109+lorenaserbai20@users.noreply.github.com>
2024-09-27 11:53:09 -0300

    dados gigantes

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,6 +3,9 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados =  await res.json()
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `Você sabia que o mundo ter ${dados.total_pessoas_mundos} de pesoas e que aproximadamente ${dados.total_pessoas_conectadas} etão conectadas em alguma rede social e passam em média ${dados.tempo.medio} horas conectadas.`
     console.log(dados);
 }
 visualizarInformacoesGlobais()
